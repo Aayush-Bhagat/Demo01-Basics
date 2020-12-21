8579b4f0a899d0cf4e5c619d136475aa5774c6cd
Aayush Bhagat <lamblesus@gmail.com>
2020-12-21 18:16:08 -0500

    made boring cube rotate

--- Assets/SpinComponenet.cs
+++ Assets/SpinComponenet.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class SpinComponenet : MonoBehaviour
 {
     private bool printedMessage = false;
+    private Transform myTransform;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello from Start");
+        myTransform = GetComponent<Transform>();
+        Debug.Log(myTransform);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        myTransform.Rotate(.1f, 0f, 0f);
         if (!printedMessage)
         {
             Debug.Log("Hello from Update");
