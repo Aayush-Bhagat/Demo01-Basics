1d527e2d3a8c323dc666334e261b54420b878c2b
Aayush Bhagat <lamblesus@gmail.com>
2020-12-21 18:00:45 -0500

    changes to Spin Component

---
+++ Assets/SpinComponenet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinComponenet : MonoBehaviour
+{
+    private bool printedMessage = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from Start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!printedMessage)
+        {
+            Debug.Log("Hello from Update");
+            printedMessage = true;
+        }
+
+    }
+}
